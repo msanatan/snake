2be2488b6e23bef329aca9bd290dcd6090912943
Marcus Sanatan <msanatan@gmail.com>
2015-11-13 19:58:48 -0400

    Snake Game object
    This will contain the functions to run the game, by first making a grid and then making a snake that can move on the grid (and of course randomly generated apples)

---
+++ javascripts/Snake.js
@@ -0,0 +1,21 @@
+var Game = Game || {};
+
+
+Game.SnakeGame = function(width, height, options) {
+  'use strict';
+  this.width = width;
+  this.height = height;
+  this.grid = [];
+};
+
+Game.SnakeGame.prototype.update = function(inputHandler) {
+  'use strict';
+};
+
+Game.SnakeGame.prototype.render = function(context) {
+  'use strict';
+  context.beginPath();
+  context.fillStyle = '#000000';
+  context.fillRect(0, 0, this.width, this.height);
+  context.closePath();
+};
