48b2cddee063bb224bb0a4776cadcd3406215d2c
Marcus Sanatan <msanatan@gmail.com>
2015-11-15 12:14:33 -0400

    Set up "grid" and create snake
    Using the blocks, set up a snake array with is adjust an array containing arrays of rows and columns. We also render the snake as well :)

--- javascripts/Snake.js
+++ javascripts/Snake.js
@@ -1,6 +1,5 @@
 var Game = Game || {};
 
-
 Game.SnakeGame = function(width, height, options) {
   'use strict';
   this.width = width;
@@ -9,10 +8,9 @@ Game.SnakeGame = function(width, height, options) {
   this.blockHeight = options.blockHeight || 20;
   var columns = Math.round(this.width / this.blockWidth);
   var rows = Math.round(this.height / this.blockHeight);
-  this.grid = new Array(columns);
-  this.grid.forEach(function(g) {
-    g = new Array(rows);
-  });
+  var midCol = Math.floor(columns / 2);
+  var midRow = Math.floor(rows / 2);
+  this.snake = [[midCol, midRow], [midCol - 1, midRow], [midCol - 2, midRow]];
 };
 
 Game.SnakeGame.prototype.DIRECTIONS = {
@@ -22,14 +20,38 @@ Game.SnakeGame.prototype.DIRECTIONS = {
   LEFT: 'left'
 };
 
+Game.SnakeGame.prototype.blockPosition = function(col, row) {
+  'use strict';
+  return {
+    x: col * this.blockWidth,
+    y: row * this.blockHeight
+  };
+};
+
 Game.SnakeGame.prototype.update = function(inputHandler) {
   'use strict';
 };
 
 Game.SnakeGame.prototype.render = function(context) {
   'use strict';
+  var coordinates;
+  var self = this;
+
   context.beginPath();
   context.fillStyle = '#000000';
   context.fillRect(0, 0, this.width, this.height);
+  context.closePath();
+
+  context.beginPath();
+  this.snake.forEach(function(s) {
+    coordinates = self.blockPosition(s[0], s[1]);
+    context.fillStyle = '#FFFFFF';
+    context.fillRect(coordinates.x, coordinates.y, self.blockWidth,
+                 self.blockHeight);
+    context.lineWidth = "1";
+    context.strokeStyle = '#000000';
+    context.strokeRect(coordinates.x, coordinates.y, self.blockWidth,
+                      self.blockHeight);
+  });
   context.closePath();
 };
